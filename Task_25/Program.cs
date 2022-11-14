5f1f7ad51a969c69df770b47a1446d468ee189c7
Klim <naiberd669@bk.ru>
2022-11-14 07:33:46 +0300

    Задача Task_25 выполнена

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -1,1 +1,18 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int MathPow(int a, int b)
+{
+    int result = a;
+    for (int i = 2; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+Console.Write("Введите число А = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B = ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = MathPow(a, b);
+Console.WriteLine($"Число {a} в степени {b} = {result}");
