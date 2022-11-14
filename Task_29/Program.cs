98b5b46e0f134c9c6be1665e5eda80008ba6a836
Klim <naiberd669@bk.ru>
2022-11-14 19:34:01 +0300

    Задание 29 выполнено

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -1,19 +1,26 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-int[] InMassive (int size, int min, int max)
-                   {
-      int[] array=new int[size];
-                    for (int i=0; i < size; i++)
-                    {
- array[i]=new Random().Next(min, max); 
-                    }
-return array;
+int[] InMassive(int size, int min, int max)
+{
+    int[] array = new int[size];
+    if (min > max)
+    {
+        int minTer = max;
+        int maxTer = min;
+        min = minTer;
+        max = maxTer;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
 
-                   }
-Console.Write("Введите размер массива = "); 
-   int size = Convert.ToInt32(Console.ReadLine());
-   Console.Write("Введите минимальный элемент массива = "); 
-   int min = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Введите максимальный элемент массива = "); 
-   int max = Convert.ToInt32(Console.ReadLine());
-    int[] array= InMassive(size, min, max);
-    Console.WriteLine($"[{String.Join(", ", array)}]");
+}
+Console.Write("Введите размер массива = ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальный элемент массива = ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальный элемент массива = ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] array = InMassive(size, min, max);
+Console.WriteLine($"[{String.Join(", ", array)}]");
